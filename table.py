ec75619b013048b9b69370827d5331faeb61b170
IbrahimAli192 <the.honouredone663@gmail.com>
2026-07-02 18:33:02 +0300

    Add files via upload

---
+++ table.py
@@ -0,0 +1,6 @@
+#python program to print the multiplication table of 67 from 1 to 10
+#loop from 1 to 10
+for i in range(1, 11):
+    #print the multiplication of 67 by i
+    print(f"67 x {i} = {67 * i}")
+    
